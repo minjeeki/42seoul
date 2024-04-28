5add0d3a999b4cb7290b63b7fb6d58c0a161fe9f
minjee kim <devnninzy@naver.com>
2024-04-29 00:03:42 +0900

    check memory leaks and fix one of them, but need to fix leaks at ft_split.c in libft directory

--- inner_2circle/push_swap/second_code/srcs/push_swap.c
+++ inner_2circle/push_swap/second_code/srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: minjeeki <minjeeki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 14:08:01 by minjeeki          #+#    #+#             */
-/*   Updated: 2024/04/28 22:34:55 by minjeeki         ###   ########seoul.kr  */
+/*   Updated: 2024/04/29 00:01:35 by minjeeki         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,11 @@ int	main(int argc, char *argv[])
 	if (size_arr <= 0)
 		return (1);
 	if (ft_check_condition_and_normalize(&origin_arr, size_arr) == 1)
+	{
+		free(origin_arr);
 		return (1);
+	}
 	ft_print_int_arr(origin_arr, size_arr);
+	free(origin_arr);
 	return (0);
 }
